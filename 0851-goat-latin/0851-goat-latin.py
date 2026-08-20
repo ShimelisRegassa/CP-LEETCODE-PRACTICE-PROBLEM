93255f2342e6301f641e3a35ea69c0d88530cc49
SHIME <shimelisregassa6@gmail.com>
2026-08-20 21:08:53 +0300

    0851-GOAT-LATIN Stats: Time: 1 ms (10.03%), Space: 19.3 MB (20.43%) - LeetHub

--- 0851-goat-latin/0851-goat-latin.py
+++ 0851-goat-latin/0851-goat-latin.py
@@ -2,12 +2,15 @@ class Solution:
     def toGoatLatin(self, sentence: str) -> str:
         num=sentence.split()
         vowel="aeiouAEIOU"
-        for i in range(len(num)):
-            if(num[i][0] in vowel):
-                num[i]=num[i]+"ma"+"a"*(i+1)
+        c=1
+        result=""
+        for i in num:
+            if i[0] in vowel:
+                result+=i+"ma"+"a"*c+" "
             else:
-                temp=num[i][1:]+num[i][0]+"ma"+("a"*(i+1))
-                num[i]=temp
-        return " ".join(num)
+                result+=i[1:]+i[0]+"ma"+"a"*c+" "
+            c+=1
+        return result.strip()
+
 
         
