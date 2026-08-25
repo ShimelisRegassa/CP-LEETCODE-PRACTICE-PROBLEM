f85769bb9707914bad74db238a62321d3622c957
SHIME <shimelisregassa6@gmail.com>
2026-08-25 15:55:24 +0300

    Time: 8 ms (83.53%), Space: 19.5 MB (5.59%) - LeetHub

--- 2670-find-the-distinct-difference-array/2670-find-the-distinct-difference-array.py
+++ 2670-find-the-distinct-difference-array/2670-find-the-distinct-difference-array.py
@@ -1,11 +1,25 @@
 class Solution:
     def distinctDifferenceArray(self, nums: List[int]) -> List[int]:
         res=[]
+        check1=set()
+        check2=set()
+        num1=[0]*len(nums)
+        num2=[0]*len(nums)
+        val1=0
+        val2=0
         for i in range(len(nums)):
-            if(i!=len(nums)-1):
-                val=len(set(nums[:i+1]))-len(set(nums[i+1:]))
-                res.append(val)
-            else:
-                val=len(set(nums[:i+1]))
-                res.append(val)
+            if(nums[i] not in check1):
+                check1.add(nums[i])
+                val1+=1
+            num1[i]=val1
+        for j in range(len(nums)-2,-1,-1):
+            if(nums[j+1] not in check2):
+                val2+=1
+                check2.add(nums[j+1])
+            num2[j]=val2
+        for i in range(len(nums)):
+            res.append(num1[i]-num2[i])
         return res
+
+            
+        
