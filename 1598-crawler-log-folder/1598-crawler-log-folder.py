3d6867033d7b80dba3245860b9c915b541b09419
SHIME <shimelisregassa6@gmail.com>
2026-08-30 21:29:47 +0300

    Time: 0 ms (100%), Space: 19.3 MB (42.34%) - LeetHub

---
+++ 1598-crawler-log-folder/1598-crawler-log-folder.py
@@ -0,0 +1,16 @@
+class Solution:
+    def minOperations(self, logs: List[str]) -> int:
+        stack=[]
+        for i in logs:
+            if(i=="../" and stack):
+                stack.pop()
+            elif(i=="./" or i=="x/"):
+                continue
+            elif(i!="../" and i!="./" and i!="x/"):
+                stack.append(i)
+            else:
+                continue
+        return len(stack)
+
+
+        
