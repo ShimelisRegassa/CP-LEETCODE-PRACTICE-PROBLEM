e812d8ada5b73e66443f6d2b627782cb64aeead4
SHIME <shimelisregassa6@gmail.com>
2026-08-31 20:35:25 +0300

    Time: 3 ms (88.25%), Space: 20 MB (85.09%) - LeetHub

--- 0641-design-circular-deque/0641-design-circular-deque.py
+++ 0641-design-circular-deque/0641-design-circular-deque.py
@@ -1,52 +1,51 @@
 class MyCircularDeque:
-
     def __init__(self, k: int):
-        self.queue=deque()
-        self.k=k       
+        self.k=k 
+        self.queue=[0]*self.k
+        self.front=0
+        self.size=0      
     def insertFront(self, value: int) -> bool:
-        if(len(self.queue)<self.k):
-            self.queue.appendleft(value)
-            return True
-        else:
+        if(self.size==self.k):
             return False
+        self.front=(self.front-1+self.k)%self.k
+        self.queue[self.front]=value
+        self.size+=1
+        return True
     def insertLast(self, value: int) -> bool:
-        if(len(self.queue)<self.k):
-            self.queue.append(value)
-            return True
-        else:
+        if(self.size==self.k):
             return False
+        rear=(self.front+self.size)%self.k
+        self.queue[rear]=value
+        self.size+=1
+        return True
     def deleteFront(self) -> bool:
-        if(len(self.queue)>0):
-            self.queue.popleft()
-            return True
-        else:
+        if((self.size)==0):
             return False
+        self.front=(self.front+1)%self.k
+        self.size-=1
+        return True  
     def deleteLast(self) -> bool:
-        if(len(self.queue)>0):
-            self.queue.pop()
-            return True
-        else:
+        if(self.size==0):
             return False
+        self.size-=1
+        return True
     def getFront(self) -> int:
-        if(len(self.queue)>0):
-            return self.queue[0]
-        else:
+        if(self.size==0):
             return -1
+        return self.queue[self.front]
     def getRear(self) -> int:
-        if(len(self.queue)>0):
-            return self.queue[-1]
-        else:
+        if(self.size==0):
             return -1
+        rear=(self.front+self.size-1)%self.k
+        return self.queue[rear]
     def isEmpty(self) -> bool:
-        if(len(self.queue)>0):
-            return False
-        else:
+        if(self.size==0):
             return True
+        return False
     def isFull(self) -> bool:
-        if(len(self.queue)==self.k):
+        if(self.size==self.k):
             return True
-        else:
-            return False
+        return False
 # Your MyCircularDeque object will be instantiated and called as such:
 # obj = MyCircularDeque(k)
 # param_1 = obj.insertFront(value)
